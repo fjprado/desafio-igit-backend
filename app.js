a97de287b432c3d2bdf0a1330e562bf274f9854a
Fernando J Prado <f.j.prado@hotmail.com>
2020-07-09 21:23:39 -0300

    Mudança do cors

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: "https://grades-igti-frontend.herokuapp.com/",
+        origin: "https://grades-igti-fronteokuapp.com/",
     })
 );
 
